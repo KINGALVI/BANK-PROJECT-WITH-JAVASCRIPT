c15bb296652de4ca43c417852116424c6bc542fe
KINGALVI <alvialmahamud@gmail.com>
2022-08-18 00:35:00 +0600

    added javascript

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 document.getElementById('btn-submit').addEventListener('click', function () {
   const emailField = document.getElementById('user-email');
   const emailOutput = emailField.value;
-  console.log(emailOutput);
+
   const passwordField = document.getElementById('user-password');
   const passwordOutput = passwordField.value;
-  console.log(passwordOutput);
+  
   if (emailOutput == 'alvialmahamud@gmail.com' && passwordOutput == 'alvi 555 alvi') {
     window.location.href = 'bank.html'
   }
